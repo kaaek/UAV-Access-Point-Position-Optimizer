1c3d5dc3b061177f96f4294d080b344880d851c8
Khalil El Kaaki <khalil.el.kaaki@gmail.com>
2025-10-25 11:34:21 +0300

    Touch-ups

--- main.m
+++ main.m
@@ -16,30 +16,27 @@ K           = 30;
 GAMMA       = 3;
 D_0         = 1;
 P_T         = 30;       % dBm
-P_N         = -91;        % dBm 
+P_N         = -91;      % dBm 
 MAX_ITER    = 50;
 TOL         = 1e-3;     % Tolerance for k-means convergence
 BW          = 40;       % MHz
+Rmin = 0.2E6;           % Set minimum bit rate
 
+% 1. Baseline solution (K-means)
 [uav_pos, user_pos] = baselineSol(M,N,AREA,H,K,GAMMA,D_0,P_T,P_N,MAX_ITER,TOL,BW);
-Rmin = 0.2E6;   % set minimum bit rate 
 
-x0 = reshape(uav_pos', [], 1);
-
-%Bounds
+% Bounds
 lb = repelem([0;0], N);
 ub = repelem([AREA;AREA], N);
 opts = optimoptions('fmincon', 'Algorithm','interior-point', 'Display','iter');
+uav_pos_flat = reshape(uav_pos', [], 1); % Flatten the matrix (needed so that it can be passed to fmincon), now looks like: [x1, y1, x2, y2, ..., xN, yN]
 
-uav_pos_flat = reshape(uav_pos', [], 1);
-
-
-obj = objective_uav(uav_pos_flat, user_pos, H, K, GAMMA, D_0, P_T, P_N, BW);
+obj = objective_uav(uav_pos_flat, user_pos, H, K, GAMMA, D_0, P_T, P_N, BW); % Helper that describes the objective function.
 [x_opt, fval] = fmincon(@(x)objective_uav(x,user_pos,H,K,GAMMA,D_0,P_T,P_N,BW), ...
                         x0, [], [], [], [], lb, ub, ...
                         @(x) nonlcon(x,user_pos,H,K,GAMMA,D_0,P_T,P_N,BW,Rmin), ...
                         opts);
-uav_pos_opt =   reshape(x_opt, 2, N).';
+uav_pos_opt =   reshape(x_opt, 2, N).'; % Builds from a flat coordinates vector a 2D matrix with the first row being the x-coordinates and the second row being the y-coordinates.
 
 
 fprintf('Optimized UAV positions (meters):\n');
