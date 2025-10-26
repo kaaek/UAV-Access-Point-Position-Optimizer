0fd9eae73259d3b0e21b28f23956001a7f1432ff
Khalil El Kaaki <khalil.el.kaaki@gmail.com>
2025-10-26 21:11:45 +0200

    Final Commit for Phase 1

--- main.m
+++ main.m
@@ -27,32 +27,32 @@ user_pos    = ceil(sqrt(AREA)) * rand(2, M);
 
 % 1. N sweep
 N_vals = [2 3 4 5 6 8 10];
-sumlink_mbps_kmeans = zeros(length(N_vals), 1);
-sumlink_mbps_grid = zeros(length(N_vals), 1);
-sumlink_mbps_random = zeros(length(N_vals), 1);
-sumlink_mbps_fm_kmeans = zeros(length(N_vals), 1);
-sumlink_mbps_fm_grid = zeros(length(N_vals), 1);
-sumlink_mbps_fm_random = zeros(length(N_vals), 1);
+sumlink_mbps_kmeans_arr = zeros(length(N_vals), 1);
+sumlink_mbps_grid_arr = zeros(length(N_vals), 1);
+sumlink_mbps_random_arr = zeros(length(N_vals), 1);
+sumlink_mbps_fm_kmeans_arr = zeros(length(N_vals), 1);
+sumlink_mbps_fm_grid_arr = zeros(length(N_vals), 1);
+sumlink_mbps_fm_random_arr = zeros(length(N_vals), 1);
 for i = 1:length(N_vals)
     n = N_vals(i);
     % [sumlink_mbps_kmeans, sumlink_mbps_grid, sumlink_mbps_random, sumlink_mbps_fm_kmeans, sumlink_mbps_fm_grid, sumlink_mbps_fm_random] = analysis(M, n, AREA, H, K, GAMMA, D_0, P_T, P_N, MAX_ITER, TOL, BW_total, Rmin, user_pos);
     [sumlink_mbps_kmeans, sumlink_mbps_grid, sumlink_mbps_random, sumlink_mbps_fm_kmeans, sumlink_mbps_fm_grid, sumlink_mbps_fm_random] = analysis(M, n, AREA, H_M, H, F, P_T, P_N, MAX_ITER, TOL, BW_total, Rmin, user_pos);
-    sumlink_mbps_kmeans(i) = sumlink_mbps_kmeans;
-    sumlink_mbps_grid(i) = sumlink_mbps_grid;
-    sumlink_mbps_random(i) = sumlink_mbps_random;
-    sumlink_mbps_fm_kmeans(i) = sumlink_mbps_fm_kmeans;
-    sumlink_mbps_fm_grid(i) = sumlink_mbps_fm_grid;
-    sumlink_mbps_fm_random(i) = sumlink_mbps_fm_random;
+    sumlink_mbps_kmeans_arr(1,i) = sumlink_mbps_kmeans;
+    sumlink_mbps_grid_arr(1,i) = sumlink_mbps_grid;
+    sumlink_mbps_random_arr(1,i) = sumlink_mbps_random;
+    sumlink_mbps_fm_kmeans_arr(1,i) = sumlink_mbps_fm_kmeans;
+    sumlink_mbps_fm_grid_arr(1,i) = sumlink_mbps_fm_grid;
+    sumlink_mbps_fm_random_arr(1,i) = sumlink_mbps_fm_random;
 end
 
 figure('Name', 'Variation of Bitrate With Number of UAVs');
 hold on;
-plot(N_vals, sumlink_mbps_kmeans, '-o', 'DisplayName', 'K-Means');
-plot(N_vals, sumlink_mbps_grid, '-s', 'DisplayName', 'Grid');
-plot(N_vals, sumlink_mbps_random, '-d', 'DisplayName', 'Random');
-plot(N_vals, sumlink_mbps_fm_kmeans, '-^', 'DisplayName', 'K-Means Optimized');
-plot(N_vals, sumlink_mbps_fm_grid, '-v', 'DisplayName', 'Grid Optimized');
-plot(N_vals, sumlink_mbps_fm_random, '-x', 'DisplayName', 'Random Optimized');
+plot(N_vals, sumlink_mbps_kmeans_arr(1,:), '-o', 'DisplayName', 'K-Means');
+plot(N_vals, sumlink_mbps_grid_arr(1,:), '-s', 'DisplayName', 'Grid');
+plot(N_vals, sumlink_mbps_random_arr(1,:), '-d', 'DisplayName', 'Random');
+plot(N_vals, sumlink_mbps_fm_kmeans_arr(1,:), '-^', 'DisplayName', 'K-Means Optimized');
+plot(N_vals, sumlink_mbps_fm_grid_arr(1,:), '-v', 'DisplayName', 'Grid Optimized');
+plot(N_vals, sumlink_mbps_fm_random_arr(1,:), '-x', 'DisplayName', 'Random Optimized');
 hold off;
 
 title('Bitrate vs Number of UAVs');
@@ -64,15 +64,15 @@ grid on;
 fprintf('--------------------------------------------------\n');
 fprintf(' BASELINE SOLUTIONS \n');
 fprintf('--------------------------------------------------\n');
-fprintf('Sum of k-means bit rate: %.2f Mbps.\n', sum(sumlink_mbps_kmeans));
-fprintf('Sum of grid-placement bit rate: %.2f Mbps.\n', sum(sumlink_mbps_grid));
-fprintf('Sum of random-placement bit rate: %.2f Mbps.\n', sum(sumlink_mbps_random));
+fprintf('mean of k-means bit rate: %.2f Mbps.\n', mean(sumlink_mbps_kmeans));
+fprintf('mean of grid-placement bit rate: %.2f Mbps.\n', mean(sumlink_mbps_grid));
+fprintf('mean of random-placement bit rate: %.2f Mbps.\n', mean(sumlink_mbps_random));
 fprintf('--------------------------------------------------\n');
 fprintf(' OPTIMIZED SOLUTIONS \n');
 fprintf('--------------------------------------------------\n');
-fprintf('Sum of k-mens bit rate after optimization: %.2f Mbps\n', sum(sumlink_mbps_fm_kmeans));
-fprintf('Sum of grid-placement bit rate after optimization: %.2f Mbps\n', sum(sumlink_mbps_fm_grid));
-fprintf('Sum of random-placement bit rate after optimization: %.2f Mbps\n', sum(sumlink_mbps_fm_random));
+fprintf('mean of k-mens bit rate after optimization: %.2f Mbps\n', mean(sumlink_mbps_fm_kmeans));
+fprintf('mean of grid-placement bit rate after optimization: %.2f Mbps\n', mean(sumlink_mbps_fm_grid));
+fprintf('mean of random-placement bit rate after optimization: %.2f Mbps\n', mean(sumlink_mbps_fm_random));
 fprintf('--------------------------------------------------\n');
 
 % % H sweep
