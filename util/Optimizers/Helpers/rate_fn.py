4a46761a75d5154e3fe54c1ec92d614135f91c8a
Khalil El Kaaki <khalil.el.kaaki@gmail.com>
2025-11-21 09:21:46 +0200

    Revert "Add Python utilities for UAV network optimization"
    This reverts commit 6d930a4f89badb4436e4cb54dbae88c73f0b8f48.

--- util/Optimizers/Helpers/rate_fn.py
+++
@@ -1,4 +0,0 @@
-"""Thin re-export of rate functions for optimizer helpers."""
-from ...Common.rate_fn import rate_shannon
-
-__all__ = ["rate_shannon"]
--- util/__init__.py
+++
@@ -1,3 +0,0 @@
-# util package
-from . import Common, Clustering, Optimizers, Plotter
-__all__ = ["Common", "Clustering", "Optimizers", "Plotter"]
